4816da8f3fbc1cd8eb4ca14606fc5678424c522a
odruke-s <odruke-s@student.42lausanne.ch>
2025-07-05 01:36:12 +0200

    finished ex04

--- MODULE_01/ex04/src/main.cpp
+++ MODULE_01/ex04/src/main.cpp
@@ -1,27 +1,70 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
 
 
-int	main()
+int	main(int ac, char **av)
 {
-	std::string		brain = "HI THIS IS BRAIN";
+	std::stringstream	buff;
+	std::string			content;
+	std::string			filename;
+	std::string			s1;
+	std::string			s2;
+	size_t				pos = 0;
 
-	std::string*	stringPTR = &brain;
-	std::string&	stringREF = brain;
+	if (ac != 4){
+		std::cout
+			<< "Wrong parameters. Usage:\n"
+			<< "./sedplus [filename.txt] [\"to replace\"] [\"to be replaced\"]" << std::endl;
+			return EXIT_FAILURE;
+	}
+	filename = av[1];
+	if (filename.empty()){
+		std::cout << "no file to open" << std::endl;
+		return EXIT_FAILURE;
+	}
+	if (!filename.compare("replaced.txt")){
+		std::cout << "can't read and write to the same file" << std::endl;
+		return EXIT_FAILURE;
+	}
 
-	std::cout
-		<< std::setw(50) << std::left << "this is the address of brain"
-		<< std::setw(17) << std::right << &brain << std::endl
-		<< std::setw(50) << std::left << "this is the aDdress held in the pointer stringPTR"
-		<< std::setw(17) << std::right << stringPTR << std::endl
-		<< std::setw(50) << std::left << "this is the aDdress held the pointer stringREF"
-		<< std::setw(17) << std::right << &stringREF << std::endl;
+	std::ifstream file(filename.c_str());
+	s1 = av[2];
+	s2 = av[3];
+	if (s1.empty()){
+		std::cout << "no text to find inputed" << std::endl;
+		return EXIT_FAILURE;
+	}
+	if (!file){
+		std::cout << "couldn't open the file" << std::endl;
+		return EXIT_FAILURE;
+	}
 
-	std::cout
-		<< std::setw(44) << std::left << "\nthis is the value of brain"
-		<< std::setw(17) << std::right << brain << std::endl
-		<< std::setw(43) << std::left << "this is the value of pointer stringPTR"
-		<< std::setw(17) << std::right << *stringPTR << std::endl
-		<< std::setw(43) << std::left << "this is the value of stringREF"
-		<< std::setw(17) << std::right << stringREF << std::endl;
+	buff << file.rdbuf();
+	content = buff.str();
+
+	while ((pos = content.find(s1, pos)) != std::string::npos){
+		content.erase(pos, s1.length());
+		content.insert(pos, s2);
+		pos += s2.length();
+	}
+	if (pos == 0) {
+		std::cout << "No occurrences of \"" << s1 << "\" found.\n";
+		file.close();
+		return EXIT_SUCCESS;
+	}
+
+	std::ofstream	outfile("replaced.txt");
+	if (!outfile){
+		file.close();
+		std::cout << "could't create/open replaced.txt" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	outfile << content;
+	file.close();
+	outfile.close();
+	return EXIT_SUCCESS;
 }
